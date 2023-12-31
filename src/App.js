7ec8240e9bb83d33a20a74f4b9636700f6a021f6
StoicNeutron <stoicneutron@Samnangs-MBP.fios-router.home>
2023-12-31 09:14:50 -0500

    add comonent library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
 import "./components/NavBar";
 import Navbar from './components/NavBar';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Button variant="contained">Sign In</Button>
     </div>
   );
 }
