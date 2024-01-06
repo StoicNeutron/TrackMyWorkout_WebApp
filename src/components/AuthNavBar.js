c90ee47837fde5e85f739f7b8f680e7d54d401a2
StoicNeutron <stoicneutron@Samnangs-MBP.fios-router.home>
2024-01-06 13:08:09 -0500

    fix compile error

--- src/components/AuthNavBar.js
+++ src/components/AuthNavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 const AuthNavbar = () => {
